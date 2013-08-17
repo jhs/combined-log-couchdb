cd0a47dccbc8a761f12e9067eff464a375868ee6
Jason Smith (work) <jason.h.smith@gmail.com>
2013-08-17 09:39:53 +0000

    Run: ./rebar/rebar create-app appid=combined_log

---
+++ src/combined_log_app.erl
@@ -0,0 +1,16 @@
+-module(combined_log_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    combined_log_sup:start_link().
+
+stop(_State) ->
+    ok.
