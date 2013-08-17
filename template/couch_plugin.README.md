85ca434fe3603685d94b2cd511a27256d3ad49f6
Jason Smith (work) <jason.h.smith@gmail.com>
2013-08-17 10:56:18 +0000

    First crack at a couch_plugin Rebar template

---
+++ template/couch_plugin.README.md
@@ -0,0 +1,8 @@
+{{name}}
+====================
+
+## Installation
+
+You will need Apache CouchDB version 1.4.0 or later.
+
+(For now you are on your own until somebody figures out how this works.)
---
+++ template/couch_plugin_srv.erl
@@ -0,0 +1,62 @@
+%%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%%    you may not use this file except in compliance with the License.
+%%%    You may obtain a copy of the License at
+%%%
+%%%         http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%%    Unless required by applicable law or agreed to in writing, software
+%%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%%    See the License for the specific language governing permissions and
+%%%    limitations under the License.
+
+-module({{name}}).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(Args) ->
+    {ok, Args}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
